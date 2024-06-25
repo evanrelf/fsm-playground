8b57a2a7abec19d12c68a89ec999319b83553ae3
Evan Relf <evan@evanrelf.com>
2024-06-24 22:52:29 -0700

    Remove fallthrough instance
    It's incompatible with the associated data families.

--- TH.hs
+++ TH.hs
@@ -18,14 +18,6 @@ class StateMachineTH t where
   data StateTag t :: Type -> Type
   data TransitionTag t :: Type -> Type -> Type
 
-type UnimplementedError t = TypeError
-  ( Text "Finish setup by calling $(derive ''" :<>: ShowType t :<>: Text ")"
-  )
-
-instance {-# OVERLAPPABLE #-} UnimplementedError t => StateMachineTH t where
-  data StateTag _ s
-  data TransitionTag _ i o
-
 -- TODO: Generate `StateMachineTH` instance
 derive :: Name -> Q [Dec]
 derive name = pure []
