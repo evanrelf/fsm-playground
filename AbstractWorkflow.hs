8a72602f91c14b656aed34de18e924e907e2f9a8
Evan Relf <evan@evanrelf.com>
2024-06-28 00:41:35 -0700

    Re-order things for reading in abstract module

--- AbstractWorkflow.hs
+++ AbstractWorkflow.hs
@@ -5,17 +5,20 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module AbstractWorkflow
-  ( SomeStateTag (..)
+  ( AbstractWorkflow (..)
+
+  , WorkflowInfo (..)
+  , StateInfo (..)
+  , TransitionInfo (..)
+  , stateInfos
+  , transitionInfos
+
+  , SomeStateTag (..)
   , SomeTransitionTag (..)
   , SomeWorkflowInfo (..)
   , SomeStateInfo (..)
   , SomeTransitionInfo (..)
-  , WorkflowInfo (..)
-  , StateInfo (..)
-  , TransitionInfo (..)
-  , AbstractWorkflow (..)
-  , stateInfos
-  , transitionInfos
+
   , AbstractState
   , initA
   , transA
@@ -25,15 +28,17 @@ where
 import Data.Kind (Type)
 import Type.Reflection (Typeable)
 
-data SomeStateTag w = forall s. SomeStateTag (StateTag w s)
-
-data SomeTransitionTag w = forall i o. SomeTransitionTag (TransitionTag w i o)
-
-data SomeWorkflowInfo = forall w. SomeWorkflowInfo (WorkflowInfo w)
-
-data SomeStateInfo = forall w. SomeStateInfo (StateInfo w)
-
-data SomeTransitionInfo = forall w. SomeTransitionInfo (TransitionInfo w)
+-- TODO: Derive instances with Template Haskell
+class AbstractWorkflow w where
+  data StateTag w :: Type -> Type
+  data TransitionTag w :: Type -> Type -> Type
+  states :: [SomeStateTag w]
+  transitions :: [SomeTransitionTag w]
+  workflowInfo :: WorkflowInfo w
+  stateInfo :: StateTag w s -> StateInfo w
+  transitionInfo :: TransitionTag w i o -> TransitionInfo w
+  stateTag' :: (Typeable a, Typeable b) => proxy a -> Maybe (StateTag w b)
+  transitionTag :: w f i o -> TransitionTag w i o
 
 data WorkflowInfo w = WorkflowInfo
   { name :: String
@@ -54,24 +59,22 @@ data TransitionInfo w = TransitionInfo
   , output :: StateInfo w
   }
 
--- TODO: Derive instances with Template Haskell
-class AbstractWorkflow w where
-  data StateTag w :: Type -> Type
-  data TransitionTag w :: Type -> Type -> Type
-  states :: [SomeStateTag w]
-  transitions :: [SomeTransitionTag w]
-  workflowInfo :: WorkflowInfo w
-  stateInfo :: StateTag w s -> StateInfo w
-  transitionInfo :: TransitionTag w i o -> TransitionInfo w
-  stateTag' :: (Typeable a, Typeable b) => proxy a -> Maybe (StateTag w b)
-  transitionTag :: w f i o -> TransitionTag w i o
-
 stateInfos :: AbstractWorkflow w => [StateInfo w]
 stateInfos = map (\(SomeStateTag tag) -> stateInfo tag) states
 
 transitionInfos :: AbstractWorkflow w => [TransitionInfo w]
 transitionInfos =
   map (\(SomeTransitionTag tag) -> transitionInfo tag) transitions
+
+data SomeStateTag w = forall s. SomeStateTag (StateTag w s)
+
+data SomeTransitionTag w = forall i o. SomeTransitionTag (TransitionTag w i o)
+
+data SomeWorkflowInfo = forall w. SomeWorkflowInfo (WorkflowInfo w)
+
+data SomeStateInfo = forall w. SomeStateInfo (StateInfo w)
+
+data SomeTransitionInfo = forall w. SomeTransitionInfo (TransitionInfo w)
 
 data AbstractState s = AbstractState
 
