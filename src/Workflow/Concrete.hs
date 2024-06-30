4c2833c740dbc7a2e177c42784c850888bb7c965
Evan Relf <evan@evanrelf.com>
2024-06-30 13:17:55 -0700

    Add `aeson` instances, tweak docs, etc.

--- src/Workflow/Concrete.hs
+++ src/Workflow/Concrete.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE UndecidableInstances #-}
 
@@ -8,11 +11,15 @@ module Workflow.Concrete
   , transC
   , State (State)
   , getState
+  , UnsafeStateFromJSON (..)
   )
 where
 
+import Data.Aeson
 import Data.Hashable (Hashable)
+import Data.Kind (Constraint)
 import GHC.Records (HasField (..))
+import GHC.TypeError (ErrorMessage (..), TypeError)
 
 -- | If you describe how to modify your underlying state data for all
 -- transitions in this workflow, then you get type-safe workflow transitions
@@ -24,21 +31,22 @@ initC :: (ConcreteWorkflow w, Functor f) => w f () o -> f (State w o)
 initC w = UnsafeState <$> transImpl w ()
 
 transC :: (ConcreteWorkflow w, Functor f) => w f i o -> State w i -> f (State w o)
-transC w (UnsafeState i) = UnsafeState <$> transImpl w i
+transC w (State i) = UnsafeState <$> transImpl w i
 
 -- | A completely opaque, correct by construction container for state. Can only
 -- be constructed by calling `initC` or `transC`.
 --
 -- In other words, `MyState` is just a regular value that you can inspect and
--- modify arbitrarily, but `State MyWorkflow MyState` is a `MyState` that is
+-- modify arbitrarily, but @'State' MyWorkflow MyState@ is a `MyState` that is
 -- guaranteed to have been produced by lawful transitions in the `MyWorkflow`
 -- workflow.
 newtype State w a = UnsafeState a
-  deriving newtype (Eq, Ord, Hashable)
+  deriving newtype (Eq, Ord, Hashable, ToJSON)
   deriving stock (Show)
 
+-- | Use all of `r`'s fields with @'State' w r@, no unwrapping necessary.
 instance HasField x r a => HasField x (State w r) a where
-  getField (UnsafeState r) = getField @x r
+  getField = getField @x . getState
 
 -- | Read-only pattern for unwrapping `State`s. Cannot be used to construct new
 -- `State`s.
@@ -46,11 +54,32 @@ instance HasField x r a => HasField x (State w r) a where
 -- Pattern matching with this is equivalent to calling `getState`.
 pattern State :: a -> State w a
 pattern State a <- UnsafeState a
+{-# COMPLETE State #-}
 -- TODO: Make this explicitly bidirectional, and add a type error on the side
 -- that tries to construct a `State`, to explain why that's not allowed?
 -- Otherwise it's a vague error from GHC about the pattern being unidirectional.
 
-{-# COMPLETE State #-}
-
 getState :: State w a -> a
-getState (UnsafeState a) = a
+getState (State a) = a
+
+data UnsafeStateFromJSON w a = UnsafeStateFromJSON (State w a)
+
+type UnsafetyError :: k1 -> k2 -> Constraint
+type UnsafetyError w a = TypeError
+  ( Text "Decoding a `State " :<>: ShowType w :<>: Text " " :<>: ShowType a :<>:
+    Text "` from JSON is unsafe! You could construct an illegal state by " :<>:
+    Text "decoding arbitrary JSON.\n" :$$: Text "If you're confident the " :<>:
+    Text "JSON represents a legal state for this workflow, you can bypass " :<>:
+    Text "this error using the `UnsafeStateFromJSON` wrapper type."
+  )
+
+-- | Decoding from JSON is unsafe! You could construct an illegal state by
+-- decoding arbitrary JSON.
+--
+-- If you're confident the JSON represents a legal state for this workflow, use
+-- the `UnsafeStateFromJSON` wrapper type.
+instance UnsafetyError w a => FromJSON (State w a) where
+  parseJSON = error "unreachable"
+
+instance (ConcreteWorkflow w, FromJSON a) => FromJSON (UnsafeStateFromJSON w a) where
+  parseJSON = fmap (UnsafeStateFromJSON . UnsafeState) . parseJSON
