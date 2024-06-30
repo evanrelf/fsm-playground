abe86a6513f9c13bf37a454834d412076cb4871d
Evan Relf <evan@evanrelf.com>
2024-06-30 14:22:30 -0700

    Move `TimeRelease` example to its own module

--- src/Examples.hs
+++ src/Examples.hs
@@ -5,59 +5,18 @@
 
 module Examples
   ( module Examples
+  , module Workflow.Example.TimeRelease
   , module Workflow.Example.Xyz
   )
 where
 
-import Data.Function ((&))
-import Data.Functor.Identity (Identity (..))
 import Data.Monoid (Sum (..))
-import Data.Proxy (Proxy (..))
 import Effectful (Eff, (:>), runPureEff)
 import Effectful.Writer.Dynamic (Writer, runWriterLocal, tell)
-import GHC.TypeLits (KnownNat, type (-))
 import Prelude hiding (init)
 import Workflow
 import Workflow.Example.Xyz
-
--- Pretend this is in its own module, and the constructor is not exported.
-newtype Box n a = UnsafeBox a
-
-data TimeRelease f i o where
-  Lock :: KnownNat n => Proxy n -> a -> TimeRelease Identity () (Box n a)
-  Tick :: TimeRelease Identity (Box n a) (Box (n - 1) a)
-  Unlock :: TimeRelease Identity (Box 0 a) a
-
-instance ConcreteWorkflow TimeRelease where
-  transImpl :: TimeRelease f i o -> i -> f o
-  transImpl = \case
-    Lock _ a -> \() -> pure (UnsafeBox a)
-    Tick -> \(UnsafeBox a) -> pure (UnsafeBox a)
-    Unlock -> \(UnsafeBox a) -> pure a
-
-lock :: forall n a. KnownNat n => a -> State TimeRelease (Box n a)
-lock a = runIdentity $ init (Lock (Proxy @n) a)
-
-tick :: State TimeRelease (Box n a) -> State TimeRelease (Box (n - 1) a)
-tick i = runIdentity $ trans Tick i
-
-unlock :: State TimeRelease (Box 0 a) -> a
-unlock i = getState $ runIdentity $ trans Unlock i
-
-_exampleTimeRelease :: Bool
-_exampleTimeRelease =
-  (42 :: Int)
-    & lock @3
-    -- Comment one of these `tick`s out and watch the program fail to compile!
-    -- GHC enforces that you call `tick` the correct number of times to
-    -- decrement the counter to zero.
-    & tick
-    & tick
-    & tick
-    & unlock
-    & (== 42)
-
---------------------------------------------------------------------------------
+import Workflow.Example.TimeRelease
 
 -- TODO: Embed a `TimeRelease` into each `TrafficLight` state, to mimic the time
 -- you wait at a traffic light, and demonstrate combining workflows.
---
+++ src/Workflow/Example/TimeRelease.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Workflow.Example.TimeRelease
+  ( Box
+  , TimeRelease (..)
+  , lock
+  , tick
+  , unlock
+  )
+where
+
+import Data.Function ((&))
+import Data.Functor.Identity (Identity (..))
+import Data.Proxy (Proxy (..))
+import GHC.TypeLits (KnownNat, type (-))
+import Prelude hiding (init)
+import Workflow
+
+newtype Box n a = UnsafeBox a
+
+data TimeRelease f i o where
+  Lock :: KnownNat n => Proxy n -> a -> TimeRelease Identity () (Box n a)
+  Tick :: TimeRelease Identity (Box n a) (Box (n - 1) a)
+  Unlock :: TimeRelease Identity (Box 0 a) a
+
+instance ConcreteWorkflow TimeRelease where
+  transImpl :: TimeRelease f i o -> i -> f o
+  transImpl = \case
+    Lock _ a -> \() -> pure (UnsafeBox a)
+    Tick -> \(UnsafeBox a) -> pure (UnsafeBox a)
+    Unlock -> \(UnsafeBox a) -> pure a
+
+lock :: forall n a. KnownNat n => a -> State TimeRelease (Box n a)
+lock a = runIdentity $ init (Lock (Proxy @n) a)
+
+tick :: State TimeRelease (Box n a) -> State TimeRelease (Box (n - 1) a)
+tick i = runIdentity $ trans Tick i
+
+unlock :: State TimeRelease (Box 0 a) -> a
+unlock i = getState $ runIdentity $ trans Unlock i
+
+_exampleTimeRelease :: Bool
+_exampleTimeRelease =
+  (42 :: Int)
+    & lock @3
+    -- Comment one of these `tick`s out and watch the program fail to compile!
+    -- GHC enforces that you call `tick` the correct number of times to
+    -- decrement the counter to zero.
+    & tick
+    & tick
+    & tick
+    & unlock
+    & (== 42)
