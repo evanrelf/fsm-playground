093c8dfc6b600fcf0de0c64b0d29465789adfbe3
Evan Relf <evan@evanrelf.com>
2024-10-24 22:40:38 -0700

    Ditch comments for now
    Too hard to maintain due to library development churn.

--- src/Workflow/Concrete.hs
+++ src/Workflow/Concrete.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE UndecidableInstances #-}
 
@@ -21,9 +19,6 @@ import Data.Kind (Constraint)
 import GHC.Records (HasField (..))
 import GHC.TypeError (ErrorMessage (..), TypeError)
 
--- | If you describe how to modify your underlying state data for all
--- transitions in this workflow, then you get type-safe workflow transitions
--- with `State` + `initC` + `transC`.
 class ConcreteWorkflow w where
   transitionRaw :: w f i o -> i -> f o
 
@@ -33,25 +28,13 @@ initialize w = UnsafeState <$> transitionRaw w ()
 transition :: (ConcreteWorkflow w, Functor f) => w f i o -> State w i -> f (State w o)
 transition w (State i) = UnsafeState <$> transitionRaw w i
 
--- | A completely opaque, correct by construction container for state. Can only
--- be constructed by calling `initC` or `transC`.
---
--- In other words, `MyState` is just a regular value that you can inspect and
--- modify arbitrarily, but @'State' MyWorkflow MyState@ is a `MyState` that is
--- guaranteed to have been produced by lawful transitions in the `MyWorkflow`
--- workflow.
 newtype State w a = UnsafeState a
   deriving newtype (Eq, Ord, Hashable, ToJSON)
   deriving stock (Show)
 
--- | Use all of `r`'s fields with @'State' w r@, no unwrapping necessary.
 instance HasField x r a => HasField x (State w r) a where
   getField = getField @x . getState
 
--- | Read-only pattern for unwrapping `State`s. Cannot be used to construct new
--- `State`s.
---
--- Pattern matching with this is equivalent to calling `getState`.
 pattern State :: a -> State w a
 pattern State a <- UnsafeState a
 {-# COMPLETE State #-}
@@ -73,11 +56,6 @@ type UnsafetyError w a = TypeError
     Text "this error using the `UnsafeStateFromJSON` wrapper type."
   )
 
--- | Decoding from JSON is unsafe! You could construct an illegal state by
--- decoding arbitrary JSON.
---
--- If you're confident the JSON represents a legal state for this workflow, use
--- the `UnsafeStateFromJSON` wrapper type.
 instance UnsafetyError w a => FromJSON (State w a) where
   parseJSON = error "unreachable"
 
